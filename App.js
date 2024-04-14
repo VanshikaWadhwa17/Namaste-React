77c0bca73510dbfe64942c70c4e18d5777fc9563
Vanshika1704 <vanshikawadhwa1704@gmail.com>
2024-04-14 17:09:51 +0530

    broken down into small functional components in react

--- App.js
+++ App.js
@@ -14,22 +14,23 @@
   import React from "react";
   import ReactDOM  from "react-dom/client";
 
-
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "First React Project"),
-    React.createElement("h2", {}, "By Vanshika Wadhwa"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-]);
-
+  const Title=()=>(
+    <h1 className="head" tabIndex="5">
+      Namaste React using JSX
+    </h1>
+  )
 //JSX
 
-console.log(parent); // object
+const HeadingComponent=()=>(
+  <>
+  <div id="container">
+    <Title/>
+    <h1 className="heading">functional component</h1>
+  </div>
+  <div id="container-2"></div>
+  </>
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent/>);
